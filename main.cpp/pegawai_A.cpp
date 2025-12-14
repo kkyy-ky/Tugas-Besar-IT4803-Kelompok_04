82180ddf2446932870798d77f0ef694af887fe31
kkyy-ky <zazkiakia439@gmail.com>
2025-12-14 15:51:54 +0700

    Update pegawai_A.cpp

--- main.cpp/pegawai_A.cpp
+++ main.cpp/pegawai_A.cpp
@@ -1,69 +1,72 @@
 #include "pegawai.h"
+#include <iostream>
+using namespace std;
 
-addrPegawai allocatePegawai(int id, string nama, string jabatan, float gaji, string jenis) {
+addrPegawai allocatePegawai(int idPegawai, string namaPegawai, string jabatan, float gaji) {
     addrPegawai P = new elmPegawai;
-
-    P->idPegawai = id;
-    P->namaPegawai = nama;
+    P->idPegawai = idPegawai;
+    P->namaPegawai = namaPegawai;
     P->jabatan = jabatan;
     P->gaji = gaji;
-    P->jenisPegawai = jenis;
-
     P->next = nullptr;
-
     return P;
 }
 
-void insertLastPegawai(listPegawai &L, addrPegawai P) {
-    if (L.first == nullptr) {
-        L.first = P;
-    } else {
-        addrPegawai Q = L.first;
-        while (Q->next != nullptr) {
-            Q = Q->next;
+void insertLastPegawai(listPegawai &LP, addrPegawai P) {
+    if (P != NULL) {
+        if (LP.first == nullptr) {
+            LP.first = P;
+        } else {
+            addrPegawai Q = LP.first;
+            while (Q->next != nullptr) {
+                Q = Q->next;
+            }
+            Q->next = P;
         }
-        Q->next = P;
     }
 }
 
-void deleteFirstPegawai(listPegawai &L, addrPegawai &P) {
-    if (L.first == nullptr) {
-        P = nullptr;
-    } else {
-        P = L.first;
-        L.first = P->next;
+void deleteFirstPegawai(listPegawai &LP, addrPegawai &P) {
+    if (LP.first != nullptr) {
+        P = LP.first;
+        LP.first = P->next;
         P->next = nullptr;
+    } else {
+        P = nullptr;
     }
 }
+
 
 void deleteAfterPegawai(addrPegawai prec, addrPegawai &P) {
     if (prec != nullptr && prec->next != nullptr) {
         P = prec->next;
         prec->next = P->next;
         P->next = nullptr;
+    } else {
+        P = nullptr;
     }
 }
 
-void showPegawai(listPegawai L) {
-    addrPegawai P = L.first;
-
-    while (P != nullptr) {
-        cout << "ID: " << P->idPegawai
-             << " | Nama: " << P->namaPegawai
-             << " | Jabatan: " << P->jabatan
-             << " | Gaji: " << P->gaji
-             << " | Jenis: " << P->jenisPegawai
-             << endl;
-
-        P = P->next;
+void showPegawai(listPegawai LP) {
+    addrPegawai P = LP.first;
+    if (P == nullptr) {
+        cout << "List pegawai kosong" << endl;
+    } else {
+        while (P != nullptr) {
+            cout << "ID Pegawai   : " << P->idPegawai << endl;
+            cout << "Nama Pegawai : " << P->namaPegawai << endl;
+            cout << "Jabatan      : " << P->jabatan << endl;
+            cout << "Gaji         : " << P->gaji << endl;
+            cout << "-----------------------------" << endl;
+            P = P->next;
+        }
     }
 }
 
-void editPegawai(addrPegawai P, string namaBaru, string jabatanBaru, float gajiBaru, string jenisBaru) {
+void editPegawai(addrPegawai P, string namaBaru, string jabatanBaru, float gajiBaru) {
     if (P != nullptr) {
         P->namaPegawai = namaBaru;
         P->jabatan = jabatanBaru;
         P->gaji = gajiBaru;
-        P->jenisPegawai = jenisBaru;
     }
 }
